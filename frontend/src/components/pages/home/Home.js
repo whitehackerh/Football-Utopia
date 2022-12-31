a4b653ec2e24c561a90d0814ae0902250a159c40
whitehackerh <hibiki.takahashi0801@gmail.com>
2023-01-01 02:20:15 +0900

    storage image

--- frontend/src/components/pages/home/Home.js
+++ frontend/src/components/pages/home/Home.js
@@ -1,3 +1,10 @@
 export default function Home() {
-    return <h1>Home</h1>
+    //return <h1>Home</h1>
+    return (
+        <>
+            <div>
+                <img src={"../../storage/user/1/profile.png"} alt="picture" />
+            </div>
+        </>
+    )
 }
